dce42e5ef658598157c49f163432bb55450189f0
NikolaevNikitaD <375298057622@yandex.ru>
2022-08-12 23:12:48 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  bracketsConfig = bracketsConfig.map((pair) => pair.join("")).join("");
+  let arr = [];
+  console.log(str);
+  for (let i = 0; i < str.length; i++) {
+      if (
+          str.charAt(i) ==
+          bracketsConfig.charAt(bracketsConfig.indexOf(str.charAt(i)) + 1)
+      )
+          if (arr[arr.length - 1] == str.charAt(i)) arr.pop();
+          else arr.push(str.charAt(i));
+      else {
+          if (bracketsConfig.indexOf(str.charAt(i)) % 2) {
+              if (
+                  bracketsConfig.charAt(
+                      bracketsConfig.indexOf(str.charAt(i)) - 1
+                  ) == arr[arr.length - 1]
+              )
+                  arr.pop();
+              else return false;
+          } else arr.push(str.charAt(i));
+      }
+  }
+
+  return !arr.length;
+};
